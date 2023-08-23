0cbb1cbd6a990f6ad7f51f363f166b75039877c5
LorenzoPastuch <lorenzopastuch@gmail.com>
2023-08-23 16:17:02 -0300

    radar

--- static/js/radar.js
+++ static/js/radar.js
@@ -12,14 +12,34 @@ function createRadarChart(labels, data) {
 
   // 2. Configuração das opções do gráfico
   const options = {
-    scale: {
-      ticks: {
+    scales: {
+      r: {
         beginAtZero: true,
+        min: 0,
+        max: 100,
+        ticks: {
+          display: false,
+          stepSize: 20,
+        },
+        grid: {
+          color: 'rgba(0, 0, 0, 1)', // Define a cor da grade
+        },
+        angleLines: {
+          color: 'rgba(0, 0, 0, 1)', // Define a cor dos eixos radiais
+        },
+        pointLabels: {
+          color : 'rgba(255, 255, 255, 1)',
+          font: {
+            size: 20, // Define o tamanho da fonte das point labels
+          },
+        },
       },
     },
-    legend: {
-      display: false, // Define para "false" para remover a legenda
-    },
+    plugins: {
+      legend: {
+        display:false
+      },
+    }
   };
 
   // 3. Criação do gráfico de radar
